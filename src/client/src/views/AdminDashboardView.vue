ff0e210f26a4616ea5cb0408609e8b6aabc0851f
Jolann <jmadec@enseirb-matmeca.fr>
2024-05-08 23:48:28 +0200

    Update AdminDashboardView.vue

--- src/client/src/views/AdminDashboardView.vue
+++ src/client/src/views/AdminDashboardView.vue
@@ -5,24 +5,28 @@
     <br>
     <div class="row">
       <div class="col-md-3" v-for="user in localUsers" :key="user._id">
-        <div class="card mb-2">
+        <div class="card mb-3">
           <div class="card-body">
             <h5 class="card-title">{{ user.email }}</h5>
             <h6 class="card-subtitle mb-2 text-muted">Role: {{ user.role }}</h6>
             <p class="card-text">Created: {{ user.date }}</p>
-            <button class="btn btn-info text-white m-1" v-if="user.email == currentUserEmail" disabled>This you</button>
+            <button class="btn btn-success text-white m-1" v-if="user.email == currentUserEmail" disabled>This you</button>
             <button class="btn btn-primary m-1" v-if="user.email !== currentUserEmail && user.email === megaAdmin" 
             @click="promoteUser(user.email)">Promote</button>
             <button class="btn btn-primary m-1" v-if="user.email !== currentUserEmail && user.email === 'admin@admin.com'" 
             disabled>Super Admin</button>
             <button class="btn btn-danger m-1" v-if="user.email !== currentUserEmail && user.email !== 'admin@admin.com' && user.role === 'admin'" 
             disabled>Admin</button>
+            <button class="btn btn-warning text-white m-1" v-if="user.email !== currentUserEmail && user.role === 'user'" 
+            disabled>User</button>
             <button class="btn btn-danger m-1" v-if="user.email !== currentUserEmail && user.role !== 'admin'" 
             @click="deleteUser(user.email)" disa>Delete</button>
             <button class="btn btn-warning text-white m-1" v-if="user.email !== currentUserEmail && megaAdmin && user.role === 'admin'" 
             @click="demoteUser(user.email)">Demote</button>
             <button class="btn btn-primary m-1 text-white" v-if="user.email !== currentUserEmail && user.role !== 'admin'" 
             @click="promoteUser(user.email)">Promote</button>
+            <button class="btn btn-danger text-white m-1" v-if="user.email !== currentUserEmail && megaAdmin && user.role === 'admin'" 
+            @click="deleteUser(user.email)">Delete</button>
           </div>
         </div>
       </div>
